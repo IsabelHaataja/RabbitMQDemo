f17af91e4631599619af0b12e5f70bb3c71390bf
Isabel Haataja <isabelhaataja@gmail.com>
2026-05-03 16:12:49 +0200

    Add project files.

---
+++ RabbitSender/Program.cs
@@ -0,0 +1,7 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new ();
+// The default username and password for RabbitMQ are "guest" and "guest" respectively.
+// fetch the connection string from environment variable/appsettings.
+factory.Uri = new Uri(uriString:"amqp://guest:guest@localhost:5672");
